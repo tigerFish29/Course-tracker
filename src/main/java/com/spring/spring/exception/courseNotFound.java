b3fedcc441173227eb5a286302063ef17a66f751
tigerFish29 <paulwchabz28@gmail.com>
2022-08-19 23:42:16 +0100

    made changes to the app

---
+++ src/main/java/com/spring/spring/exception/courseNotFound.java
@@ -0,0 +1,11 @@
+package com.spring.spring.exception;
+
+public class courseNotFound extends RuntimeException {
+
+    private static final long serialVersionUID = 5071646428281007896L;
+
+    public courseNotFound(String message) {
+        super(message);
+    }
+    
+}
